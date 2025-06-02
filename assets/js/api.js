6df029584a0ed0d17803467fccb6c451735d8e54
Meda Mondonga <106118468+medamondonga@users.noreply.github.com>
2025-06-02 13:01:07 +0100

    modal ajouter categorie

--- assets/js/api.js
+++ assets/js/api.js
@@ -12,3 +12,19 @@ export async function getArticles() {
     return [];
   }
 }
+
+export async function getCategories() {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/stock/categorie/list/", {
+      headers: {
+        Authorization: "Bearer VOTRE_TOKEN_ICY"
+      }
+    });
+    if (!response.ok) throw new Error("Erreur lors du chargement des articles");
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
