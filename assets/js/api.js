d04bedae9e3194a7498e660a921c8e04304ad030
Meda Mondonga <106118468+medamondonga@users.noreply.github.com>
2025-06-06 15:05:10 +0100

    Section Client

--- assets/js/api.js
+++ assets/js/api.js
@@ -28,3 +28,18 @@ export async function getCategories() {
   }
 }
 
+export async function getClients() {
+  try {
+    const response = await fetch("http://127.0.0.1:8000/stock/clients/", {
+      headers: {
+        Authorization: "Bearer VOTRE_TOKEN_ICY"
+      }
+    });
+    if (!response.ok) throw new Error("Erreur lors du chargement des articles");
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
